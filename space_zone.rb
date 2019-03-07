4b5c0d57b344dffa4a1dd9321fd217ce753858ac
joao Sanches <joao.carrelo.sanches@gmail.com>
2019-03-07 11:47:12 +0000

    new game space

---
+++ space_zone.rb
@@ -0,0 +1,22 @@
+require 'gosu'
+
+class Game < Gosu::Window
+
+  def initialize
+    super 640, 480
+    self.caption = 'Space Zone'
+
+  end
+
+  def update
+
+  end
+
+  def draw
+
+  end
+
+end
+
+
+Game.new.show
